e63700e0c27dd6ec620f870aa2ebb7a03dfd3d82
Yash Rajput <to.yashrajput@gmail.com>
2024-07-08 15:21:12 -0400

    Finished Design + Began API Config

---
+++ weather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+export function getWeather(lat, lon, timezone) {
+  return axios.get(
+    "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+    {
+      parems: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+      },
+    }
+  );
+}
